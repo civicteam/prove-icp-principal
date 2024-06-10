1b805dfcc19cfee8c6a07f0f086d2b8c35e6d508
TYRONE AVNIT <tyronemichael@gmail.com>
2024-06-10 14:50:11 -0500

    TECH-152 - Add support for dynamic imports

--- src/icp-sig-verifier.nodejs/sig_verifier_js.js
+++ src/icp-sig-verifier.nodejs/sig_verifier_js.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 let imports = {};
 imports['__wbindgen_placeholder__'] = module.exports;
 let wasm;
@@ -10,15 +11,15 @@ cachedTextDecoder.decode();
 let cachedUint8Memory0 = null;
 
 function getUint8Memory0() {
-    if (cachedUint8Memory0 === null || cachedUint8Memory0.byteLength === 0) {
-        cachedUint8Memory0 = new Uint8Array(wasm.memory.buffer);
-    }
-    return cachedUint8Memory0;
+  if (cachedUint8Memory0 === null || cachedUint8Memory0.byteLength === 0) {
+    cachedUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+  }
+  return cachedUint8Memory0;
 }
 
 function getStringFromWasm0(ptr, len) {
-    ptr = ptr >>> 0;
-    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+  ptr = ptr >>> 0;
+  return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
 }
 
 const heap = new Array(128).fill(undefined);
@@ -28,247 +29,258 @@ heap.push(undefined, null, true, false);
 let heap_next = heap.length;
 
 function addHeapObject(obj) {
-    if (heap_next === heap.length) heap.push(heap.length + 1);
-    const idx = heap_next;
-    heap_next = heap[idx];
+  if (heap_next === heap.length) heap.push(heap.length + 1);
+  const idx = heap_next;
+  heap_next = heap[idx];
 
-    heap[idx] = obj;
-    return idx;
+  heap[idx] = obj;
+  return idx;
 }
 
 let WASM_VECTOR_LEN = 0;
 
 function passArray8ToWasm0(arg, malloc) {
-    const ptr = malloc(arg.length * 1, 1) >>> 0;
-    getUint8Memory0().set(arg, ptr / 1);
-    WASM_VECTOR_LEN = arg.length;
-    return ptr;
+  const ptr = malloc(arg.length * 1, 1) >>> 0;
+  getUint8Memory0().set(arg, ptr / 1);
+  WASM_VECTOR_LEN = arg.length;
+  return ptr;
 }
 
 let cachedInt32Memory0 = null;
 
 function getInt32Memory0() {
-    if (cachedInt32Memory0 === null || cachedInt32Memory0.byteLength === 0) {
-        cachedInt32Memory0 = new Int32Array(wasm.memory.buffer);
-    }
-    return cachedInt32Memory0;
+  if (cachedInt32Memory0 === null || cachedInt32Memory0.byteLength === 0) {
+    cachedInt32Memory0 = new Int32Array(wasm.memory.buffer);
+  }
+  return cachedInt32Memory0;
 }
 
-function getObject(idx) { return heap[idx]; }
+function getObject(idx) {
+  return heap[idx];
+}
 
 function dropObject(idx) {
-    if (idx < 132) return;
-    heap[idx] = heap_next;
-    heap_next = idx;
+  if (idx < 132) return;
+  heap[idx] = heap_next;
+  heap_next = idx;
 }
 
 function takeObject(idx) {
-    const ret = getObject(idx);
-    dropObject(idx);
-    return ret;
+  const ret = getObject(idx);
+  dropObject(idx);
+  return ret;
 }
 /**
-* Verifies a basic (i.e. not a canister signature) IC supported signature.
-* Supported signature schemes: https://internetcomputer.org/docs/current/references/ic-interface-spec/#signatures
-*
-* Throws an error if the signature verification fails.
-* @param {Uint8Array} msg
-* @param {Uint8Array} signature
-* @param {Uint8Array} public_key
-*/
-module.exports.verifyBasicSignature = function(msg, signature, public_key) {
-    try {
-        const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-        const ptr0 = passArray8ToWasm0(msg, wasm.__wbindgen_malloc);
-        const len0 = WASM_VECTOR_LEN;
-        const ptr1 = passArray8ToWasm0(signature, wasm.__wbindgen_malloc);
-        const len1 = WASM_VECTOR_LEN;
-        const ptr2 = passArray8ToWasm0(public_key, wasm.__wbindgen_malloc);
-        const len2 = WASM_VECTOR_LEN;
-        wasm.verifyBasicSignature(retptr, ptr0, len0, ptr1, len1, ptr2, len2);
-        var r0 = getInt32Memory0()[retptr / 4 + 0];
-        var r1 = getInt32Memory0()[retptr / 4 + 1];
-        if (r1) {
-            throw takeObject(r0);
-        }
-    } finally {
-        wasm.__wbindgen_add_to_stack_pointer(16);
+ * Verifies a basic (i.e. not a canister signature) IC supported signature.
+ * Supported signature schemes: https://internetcomputer.org/docs/current/references/ic-interface-spec/#signatures
+ *
+ * Throws an error if the signature verification fails.
+ * @param {Uint8Array} msg
+ * @param {Uint8Array} signature
+ * @param {Uint8Array} public_key
+ */
+module.exports.verifyBasicSignature = function (msg, signature, public_key) {
+  try {
+    const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
+    const ptr0 = passArray8ToWasm0(msg, wasm.__wbindgen_malloc);
+    const len0 = WASM_VECTOR_LEN;
+    const ptr1 = passArray8ToWasm0(signature, wasm.__wbindgen_malloc);
+    const len1 = WASM_VECTOR_LEN;
+    const ptr2 = passArray8ToWasm0(public_key, wasm.__wbindgen_malloc);
+    const len2 = WASM_VECTOR_LEN;
+    wasm.verifyBasicSignature(retptr, ptr0, len0, ptr1, len1, ptr2, len2);
+    var r0 = getInt32Memory0()[retptr / 4 + 0];
+    var r1 = getInt32Memory0()[retptr / 4 + 1];
+    if (r1) {
+      throw takeObject(r0);
     }
+  } finally {
+    wasm.__wbindgen_add_to_stack_pointer(16);
+  }
 };
 
 /**
-* Verifies an IC canister signature.
-* More details: https://internetcomputer.org/docs/current/references/ic-interface-spec/#canister-signatures
-*
-* Throws an error if the signature verification fails.
-* @param {Uint8Array} message
-* @param {Uint8Array} signature
-* @param {Uint8Array} public_key
-* @param {Uint8Array} ic_root_public_key
-*/
-module.exports.verifyCanisterSignature = function(message, signature, public_key, ic_root_public_key) {
-    try {
-        const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-        const ptr0 = passArray8ToWasm0(message, wasm.__wbindgen_malloc);
-        const len0 = WASM_VECTOR_LEN;
-        const ptr1 = passArray8ToWasm0(signature, wasm.__wbindgen_malloc);
-        const len1 = WASM_VECTOR_LEN;
-        const ptr2 = passArray8ToWasm0(public_key, wasm.__wbindgen_malloc);
-        const len2 = WASM_VECTOR_LEN;
-        const ptr3 = passArray8ToWasm0(ic_root_public_key, wasm.__wbindgen_malloc);
-        const len3 = WASM_VECTOR_LEN;
-        wasm.verifyCanisterSignature(retptr, ptr0, len0, ptr1, len1, ptr2, len2, ptr3, len3);
-        var r0 = getInt32Memory0()[retptr / 4 + 0];
-        var r1 = getInt32Memory0()[retptr / 4 + 1];
-        if (r1) {
-            throw takeObject(r0);
-        }
-    } finally {
-        wasm.__wbindgen_add_to_stack_pointer(16);
+ * Verifies an IC canister signature.
+ * More details: https://internetcomputer.org/docs/current/references/ic-interface-spec/#canister-signatures
+ *
+ * Throws an error if the signature verification fails.
+ * @param {Uint8Array} message
+ * @param {Uint8Array} signature
+ * @param {Uint8Array} public_key
+ * @param {Uint8Array} ic_root_public_key
+ */
+module.exports.verifyCanisterSignature = function (message, signature, public_key, ic_root_public_key) {
+  try {
+    const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
+    const ptr0 = passArray8ToWasm0(message, wasm.__wbindgen_malloc);
+    const len0 = WASM_VECTOR_LEN;
+    const ptr1 = passArray8ToWasm0(signature, wasm.__wbindgen_malloc);
+    const len1 = WASM_VECTOR_LEN;
+    const ptr2 = passArray8ToWasm0(public_key, wasm.__wbindgen_malloc);
+    const len2 = WASM_VECTOR_LEN;
+    const ptr3 = passArray8ToWasm0(ic_root_public_key, wasm.__wbindgen_malloc);
+    const len3 = WASM_VECTOR_LEN;
+    wasm.verifyCanisterSignature(retptr, ptr0, len0, ptr1, len1, ptr2, len2, ptr3, len3);
+    var r0 = getInt32Memory0()[retptr / 4 + 0];
+    var r1 = getInt32Memory0()[retptr / 4 + 1];
+    if (r1) {
+      throw takeObject(r0);
     }
+  } finally {
+    wasm.__wbindgen_add_to_stack_pointer(16);
+  }
 };
 
 /**
-* Verifies any IC supported signature.
-* Supported signature schemes: https://internetcomputer.org/docs/current/references/ic-interface-spec/#signatures
-*
-* Throws an error if the signature verification fails.
-* @param {Uint8Array} message
-* @param {Uint8Array} signature
-* @param {Uint8Array} public_key
-* @param {Uint8Array} ic_root_public_key
-*/
-module.exports.verifyIcSignature = function(message, signature, public_key, ic_root_public_key) {
-    try {
-        const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-        const ptr0 = passArray8ToWasm0(message, wasm.__wbindgen_malloc);
-        const len0 = WASM_VECTOR_LEN;
-        const ptr1 = passArray8ToWasm0(signature, wasm.__wbindgen_malloc);
-        const len1 = WASM_VECTOR_LEN;
-        const ptr2 = passArray8ToWasm0(public_key, wasm.__wbindgen_malloc);
-        const len2 = WASM_VECTOR_LEN;
-        const ptr3 = passArray8ToWasm0(ic_root_public_key, wasm.__wbindgen_malloc);
-        const len3 = WASM_VECTOR_LEN;
-        wasm.verifyIcSignature(retptr, ptr0, len0, ptr1, len1, ptr2, len2, ptr3, len3);
-        var r0 = getInt32Memory0()[retptr / 4 + 0];
-        var r1 = getInt32Memory0()[retptr / 4 + 1];
-        if (r1) {
-            throw takeObject(r0);
-        }
-    } finally {
-        wasm.__wbindgen_add_to_stack_pointer(16);
+ * Verifies any IC supported signature.
+ * Supported signature schemes: https://internetcomputer.org/docs/current/references/ic-interface-spec/#signatures
+ *
+ * Throws an error if the signature verification fails.
+ * @param {Uint8Array} message
+ * @param {Uint8Array} signature
+ * @param {Uint8Array} public_key
+ * @param {Uint8Array} ic_root_public_key
+ */
+module.exports.verifyIcSignature = function (message, signature, public_key, ic_root_public_key) {
+  try {
+    const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
+    const ptr0 = passArray8ToWasm0(message, wasm.__wbindgen_malloc);
+    const len0 = WASM_VECTOR_LEN;
+    const ptr1 = passArray8ToWasm0(signature, wasm.__wbindgen_malloc);
+    const len1 = WASM_VECTOR_LEN;
+    const ptr2 = passArray8ToWasm0(public_key, wasm.__wbindgen_malloc);
+    const len2 = WASM_VECTOR_LEN;
+    const ptr3 = passArray8ToWasm0(ic_root_public_key, wasm.__wbindgen_malloc);
+    const len3 = WASM_VECTOR_LEN;
+    wasm.verifyIcSignature(retptr, ptr0, len0, ptr1, len1, ptr2, len2, ptr3, len3);
+    var r0 = getInt32Memory0()[retptr / 4 + 0];
+    var r1 = getInt32Memory0()[retptr / 4 + 1];
+    if (r1) {
+      throw takeObject(r0);
     }
+  } finally {
+    wasm.__wbindgen_add_to_stack_pointer(16);
+  }
 };
 
 let cachedTextEncoder = new TextEncoder('utf-8');
 
-const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
+const encodeString =
+  typeof cachedTextEncoder.encodeInto === 'function'
     ? function (arg, view) {
-    return cachedTextEncoder.encodeInto(arg, view);
-}
+        return cachedTextEncoder.encodeInto(arg, view);
+      }
     : function (arg, view) {
-    const buf = cachedTextEncoder.encode(arg);
-    view.set(buf);
-    return {
-        read: arg.length,
-        written: buf.length
-    };
-});
+        const buf = cachedTextEncoder.encode(arg);
+        view.set(buf);
+        return {
+          read: arg.length,
+          written: buf.length,
+        };
+      };
 
 function passStringToWasm0(arg, malloc, realloc) {
-
-    if (realloc === undefined) {
-        const buf = cachedTextEncoder.encode(arg);
-        const ptr = malloc(buf.length, 1) >>> 0;
-        getUint8Memory0().subarray(ptr, ptr + buf.length).set(buf);
-        WASM_VECTOR_LEN = buf.length;
-        return ptr;
-    }
-
-    let len = arg.length;
-    let ptr = malloc(len, 1) >>> 0;
-
-    const mem = getUint8Memory0();
-
-    let offset = 0;
-
-    for (; offset < len; offset++) {
-        const code = arg.charCodeAt(offset);
-        if (code > 0x7F) break;
-        mem[ptr + offset] = code;
-    }
-
-    if (offset !== len) {
-        if (offset !== 0) {
-            arg = arg.slice(offset);
-        }
-        ptr = realloc(ptr, len, len = offset + arg.length * 3, 1) >>> 0;
-        const view = getUint8Memory0().subarray(ptr + offset, ptr + len);
-        const ret = encodeString(arg, view);
-
-        offset += ret.written;
-    }
-
-    WASM_VECTOR_LEN = offset;
+  if (realloc === undefined) {
+    const buf = cachedTextEncoder.encode(arg);
+    const ptr = malloc(buf.length, 1) >>> 0;
+    getUint8Memory0()
+      .subarray(ptr, ptr + buf.length)
+      .set(buf);
+    WASM_VECTOR_LEN = buf.length;
     return ptr;
+  }
+
+  let len = arg.length;
+  let ptr = malloc(len, 1) >>> 0;
+
+  const mem = getUint8Memory0();
+
+  let offset = 0;
+
+  for (; offset < len; offset++) {
+    const code = arg.charCodeAt(offset);
+    if (code > 0x7f) break;
+    mem[ptr + offset] = code;
+  }
+
+  if (offset !== len) {
+    if (offset !== 0) {
+      arg = arg.slice(offset);
+    }
+    ptr = realloc(ptr, len, (len = offset + arg.length * 3), 1) >>> 0;
+    const view = getUint8Memory0().subarray(ptr + offset, ptr + len);
+    const ret = encodeString(arg, view);
+
+    offset += ret.written;
+  }
+
+  WASM_VECTOR_LEN = offset;
+  return ptr;
 }
 /**
-* Verifies the validity of the given signed delegation chain wrt. the challenge, and the other parameters.
-* Specifically:
-*  * `signed_delegation_chain` contains exactly one delegation, denoted below as `delegations[0]`
-*  * `delegations[0].pubkey` equals `challenge` (i.e. challenge is the "session key")
-*  * `signed_delegation_chain.publicKey` is a public key for canister signatures of `ii_canister_id`
-*  * `current_time_ns` denotes point in time before `delegations[0].expiration`
-*  *  TODO: `current_time_ns` denotes point in time that is not more than 5min after signature creation time
-*     (as specified in the certified tree of the Certificate embedded in the signature)
-*  * `delegations[0].signature` is a valid canister signature on a representation-independent hash of `delegations[0]`,
-*    wrt. `signed_delegation_chain.publicKey` and `ic_root_public_key_raw`
-*
-* On success returns textual representation of the self-authenticating Principal determined by
-* public key `signed_delegation_chain.publicKey` (which identifies the user).
-* @param {Uint8Array} challenge
-* @param {string} signed_delegation_chain_json
-* @param {bigint} current_time_ns
-* @param {string} ii_canister_id
-* @param {Uint8Array} ic_root_public_key_raw
-* @returns {string}
-*/
-module.exports.validateDelegationAndGetPrincipal = function(challenge, signed_delegation_chain_json, current_time_ns, ii_canister_id, ic_root_public_key_raw) {
-    let deferred6_0;
-    let deferred6_1;
-    try {
-        const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-        const ptr0 = passArray8ToWasm0(challenge, wasm.__wbindgen_malloc);
-        const len0 = WASM_VECTOR_LEN;
-        const ptr1 = passStringToWasm0(signed_delegation_chain_json, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-        const len1 = WASM_VECTOR_LEN;
-        const ptr2 = passStringToWasm0(ii_canister_id, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-        const len2 = WASM_VECTOR_LEN;
-        const ptr3 = passArray8ToWasm0(ic_root_public_key_raw, wasm.__wbindgen_malloc);
-        const len3 = WASM_VECTOR_LEN;
-        wasm.validateDelegationAndGetPrincipal(retptr, ptr0, len0, ptr1, len1, current_time_ns, ptr2, len2, ptr3, len3);
-        var r0 = getInt32Memory0()[retptr / 4 + 0];
-        var r1 = getInt32Memory0()[retptr / 4 + 1];
-        var r2 = getInt32Memory0()[retptr / 4 + 2];
-        var r3 = getInt32Memory0()[retptr / 4 + 3];
-        var ptr5 = r0;
-        var len5 = r1;
-        if (r3) {
-            ptr5 = 0; len5 = 0;
-            throw takeObject(r2);
-        }
-        deferred6_0 = ptr5;
-        deferred6_1 = len5;
-        return getStringFromWasm0(ptr5, len5);
-    } finally {
-        wasm.__wbindgen_add_to_stack_pointer(16);
-        wasm.__wbindgen_free(deferred6_0, deferred6_1, 1);
+ * Verifies the validity of the given signed delegation chain wrt. the challenge, and the other parameters.
+ * Specifically:
+ *  * `signed_delegation_chain` contains exactly one delegation, denoted below as `delegations[0]`
+ *  * `delegations[0].pubkey` equals `challenge` (i.e. challenge is the "session key")
+ *  * `signed_delegation_chain.publicKey` is a public key for canister signatures of `ii_canister_id`
+ *  * `current_time_ns` denotes point in time before `delegations[0].expiration`
+ *  *  TODO: `current_time_ns` denotes point in time that is not more than 5min after signature creation time
+ *     (as specified in the certified tree of the Certificate embedded in the signature)
+ *  * `delegations[0].signature` is a valid canister signature on a representation-independent hash of `delegations[0]`,
+ *    wrt. `signed_delegation_chain.publicKey` and `ic_root_public_key_raw`
+ *
+ * On success returns textual representation of the self-authenticating Principal determined by
+ * public key `signed_delegation_chain.publicKey` (which identifies the user).
+ * @param {Uint8Array} challenge
+ * @param {string} signed_delegation_chain_json
+ * @param {bigint} current_time_ns
+ * @param {string} ii_canister_id
+ * @param {Uint8Array} ic_root_public_key_raw
+ * @returns {string}
+ */
+module.exports.validateDelegationAndGetPrincipal = function (
+  challenge,
+  signed_delegation_chain_json,
+  current_time_ns,
+  ii_canister_id,
+  ic_root_public_key_raw
+) {
+  let deferred6_0;
+  let deferred6_1;
+  try {
+    const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
+    const ptr0 = passArray8ToWasm0(challenge, wasm.__wbindgen_malloc);
+    const len0 = WASM_VECTOR_LEN;
+    const ptr1 = passStringToWasm0(signed_delegation_chain_json, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+    const len1 = WASM_VECTOR_LEN;
+    const ptr2 = passStringToWasm0(ii_canister_id, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+    const len2 = WASM_VECTOR_LEN;
+    const ptr3 = passArray8ToWasm0(ic_root_public_key_raw, wasm.__wbindgen_malloc);
+    const len3 = WASM_VECTOR_LEN;
+    wasm.validateDelegationAndGetPrincipal(retptr, ptr0, len0, ptr1, len1, current_time_ns, ptr2, len2, ptr3, len3);
+    var r0 = getInt32Memory0()[retptr / 4 + 0];
+    var r1 = getInt32Memory0()[retptr / 4 + 1];
+    var r2 = getInt32Memory0()[retptr / 4 + 2];
+    var r3 = getInt32Memory0()[retptr / 4 + 3];
+    var ptr5 = r0;
+    var len5 = r1;
+    if (r3) {
+      ptr5 = 0;
+      len5 = 0;
+      throw takeObject(r2);
     }
+    deferred6_0 = ptr5;
+    deferred6_1 = len5;
+    return getStringFromWasm0(ptr5, len5);
+  } finally {
+    wasm.__wbindgen_add_to_stack_pointer(16);
+    wasm.__wbindgen_free(deferred6_0, deferred6_1, 1);
+  }
 };
 
-module.exports.__wbindgen_string_new = function(arg0, arg1) {
-    const ret = getStringFromWasm0(arg0, arg1);
-    return addHeapObject(ret);
+module.exports.__wbindgen_string_new = function (arg0, arg1) {
+  const ret = getStringFromWasm0(arg0, arg1);
+  return addHeapObject(ret);
 };
 
 const path = require('path').join(__dirname, 'sig_verifier_js_bg.wasm');
@@ -278,4 +290,3 @@ const wasmModule = new WebAssembly.Module(bytes);
 const wasmInstance = new WebAssembly.Instance(wasmModule, imports);
 wasm = wasmInstance.exports;
 module.exports.__wasm = wasm;
-
